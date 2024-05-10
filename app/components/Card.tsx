5ba90a2f24b331e1d7c84de0eb140d2964b1bda8
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2024-05-10 14:08:05 +0530

    .

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 interface props {
-    image : string,
-    btnText : string
+  image: string;
+  btnText: string;
 }
-const Card : React.FC<{data:props}> = ({data}) => {
+const Card: React.FC<{ data: props }> = ({ data }) => {
   return (
-    <div className='relative rounded-lg  '>
-        <img src={data?.image} alt=""  className=' rounded-md w-[350px] h-[350px]'/>
-         <div className='absolute bottom-4 flex justify-center items-center  w-full'><Button btnColor="red" text={data?.btnText.toUpperCase()} /></div>
+    <div className="relative rounded-lg  ">
+      <Image
+        src={data?.image}
+        alt=""
+        width={350}
+        height={350}
+        className=" rounded-md w-[350px] h-[350px]"
+      />
+      <div className="absolute bottom-4 flex justify-center items-center  w-full">
+        <Button btnColor="red" text={data?.btnText.toUpperCase()} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
